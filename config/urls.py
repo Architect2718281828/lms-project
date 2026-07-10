ec2d7ed31ca16404d481f0ba107739170574402d
Architect2718281828 <daniilrasitov41@gmail.com>
2026-07-10 18:03:43 +0300

    Force fix paths

--- config/urls.py
+++ config/urls.py
@@ -2,20 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-# Импортируем класс для перенаправления страниц
-from django.views.generic import RedirectView
 
 urlpatterns = [
     # Главная админка Django
     path('admin/', admin.site.urls),
 
-    # Если пользователь заходит на чистый домен "", плавно перекидываем его на "/lessons/"
-    path('', RedirectView.as_view(url='lessons/', permanent=True)),
-
-    # Подключаем все маршруты приложения к корню сайта
+    # Подключаем все маршруты приложения к корню сайта (здесь теперь лежит и главная, и логин)
     path('', include('courses.urls')),
 ]
 
-# Добавляем эти строки для корректной работы медиафайлов в режиме DEBUG
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
